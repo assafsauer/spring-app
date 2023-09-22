66c42ba4110bd52de8b23bfdfeb5cc6c40dbce04
assafsauer <as.sauer@gmail.com>
2023-09-22 17:30:52 +0200

    Update DemoController.java

--- spring/src/main/java/com/example/demo/web/DemoController.java
+++ spring/src/main/java/com/example/demo/web/DemoController.java
@@ -16,7 +16,7 @@ public class DemoController {
 
     private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping(value = { "/", "/greeting8" })
+    @GetMapping(value = { "/", "/greeting9" })
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
